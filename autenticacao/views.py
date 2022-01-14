ca13874682f1b2e87ae601ac0f0ac0ee4d5be692
rogeriodelphi <rogeriodelphi@gmail.com>
2022-01-13 23:58:08 -0300

    Adicionando mensagens a nossa aplicação

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -11,20 +13,23 @@ def cadastro(request):
         senha = request.POST.get('senha')
 
         if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preenha os todos os campos.')
             return redirect('/auth/cadastro')
 
         user = User.objects.filter(username=username)
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
             return redirect('/auth/cadastro')
         try:
             user = User(username=username,
                         email=email,
                         password=senha)
             user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
             return redirect('/auth/logar')
         except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema.')
             return redirect('auth/cadastro')
-        return HttpResponse(f'{username} {email} {senha}')
 
 def logar(request):
     return HttpResponse('Logar')
