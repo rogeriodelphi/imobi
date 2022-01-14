3b273f6043acfb936e5822dcdcfe4fc064c6c468
rogeriodelphi <rogeriodelphi@gmail.com>
2022-01-13 23:10:26 -0300

    Melhorando a autenticação

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            return redirect('/auth/cadastro')
+
+        return HttpResponse(f'{username} {senha}')
 
 def logar(request):
     return HttpResponse('Logar')
