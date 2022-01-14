20a7a085b228fbb7008b9cae45e6caa94c40cc58
rogeriodelphi <rogeriodelphi@gmail.com>
2022-01-14 01:03:24 -0300

    Realizando login na Aplicação

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,9 +3,12 @@ from django.contrib.messages import constants
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('username')
@@ -21,7 +24,7 @@ def cadastro(request):
             messages.add_message(request, constants.ERROR, 'Usuário já existe.')
             return redirect('/auth/cadastro')
         try:
-            user = User(username=username,
+            user = User.objects.create_user(username=username,
                         email=email,
                         password=senha)
             user.save()
@@ -29,7 +32,23 @@ def cadastro(request):
             return redirect('/auth/logar')
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno do sistema.')
-            return redirect('auth/cadastro')
+            return redirect('/auth/cadastro')
 
 def logar(request):
-    return HttpResponse('Logar')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
+
